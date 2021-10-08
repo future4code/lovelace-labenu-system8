b12b2585f686609c7de10d281cef795f7336de7b
gazua <gazua@localhost.localdomain>
2021-10-07 22:28:40 -0300

    Etapas finais do backend

--- src/endpoints/students/createStudent.ts
+++ src/endpoints/students/createStudent.ts
@@ -1,19 +1,37 @@
-import { con } from "../data/connection";
+import {Request, Response} from "express";
+import { con } from "../../data/connection";
 
 export const createStudent = async(req: Request, res: Response)=>{
     try{
 
-      const {name, email, birth} = req.body
+      const {name, email, birth, classId} = req.body
+      const date = birth.split("/")
+      const converted = `${date[2]}-${date[1]}-${date[0]}`
+      const millisecondsAge = Date.now() - new Date(converted).getTime()
+      const age = Math.floor(millisecondsAge / 1000 / 60 / 60 / 24 / 365)
 
       if(!name || !email || !birth){
         throw new Error('Preencha todos os campos.')
       }
 
-      const id = Math.random().substring(2)
+      if(age < 18){
+        throw new Error('Necessário ser maior de idade.')
+      }
 
-      await con.raw(`insert into students`)
+      /*      EXCEDẼNCIA DE ALUNOS POR TURMA VERIFICAR NO FRONT
+      const classmateNumber = await con.raw(`select * from students`)
+      const exceedance = classmateNumber[0].length
+      console.log(exceedance)
+      */
+      await con("students").insert({
+        name,
+        email,
+        birth: converted,
+        class_id: classId
+      })
+res.end()
 
     }catch(e){
-      res.statud(400).send({message: e.message || e.sqlMessage})
+      res.status(400).send({message: e.message || e.sqlMessage})
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express from 'express'
 import cors from 'cors'
-import { createClass } from "./endpoints/class/createClass";
+//import {createClass} from "./endpoints/class/createClass"
+import { createTeacher } from "./endpoints/teachers/createTeacher"
+//import {createStudent} from "./endpoints/students/createStudent"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 
-app.post('/class', createClass)
-
+//app.post("/class", createClass)
+app.post("/teacher", createTeacher)
+//app.post ("/student", createStudent)
 
 
 
